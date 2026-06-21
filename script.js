02cf9942faca9c19866f68d243ef031da85e2c58
Davidokt <85350280+Dav1dushka@users.noreply.github.com>
2026-06-21 10:42:03 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const cityInput = document.getElementById("city");
+const searchBtn = document.getElementById("searchBtn");
+const weatherDiv = document.getElementById("weather");
+
+searchBtn.addEventListener("click", getWeather);
+
+async function getWeather() {
+
+    const city = cityInput.value;
+
+    try {
+
+        const geoResponse = await fetch(
+            `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
+        );
+
+        const geoData = await geoResponse.json();
+
+        if (!geoData.results) {
+            weatherDiv.innerHTML = "Город не найден";
+            return;
+        }
+
+        const { latitude, longitude, name } = geoData.results[0];
+
+        const weatherResponse = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m`
+        );
+
+        const weatherData = await weatherResponse.json();
+
+        weatherDiv.innerHTML = `
+            <h2>${name}</h2>
+            <p>Температура: ${weatherData.current.temperature_2m}°C</p>
+        `;
+
+    } catch(error) {
+        weatherDiv.innerHTML = "Ошибка загрузки данных";
+    }
+}
